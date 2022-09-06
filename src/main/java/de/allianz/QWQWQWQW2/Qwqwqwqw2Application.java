c42d2ccd61ac926ba71fe6130d7613ee6dfb0a76
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-06 10:28:53 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/QWQWQWQW2/Qwqwqwqw2Application.java
@@ -0,0 +1,13 @@
+package de.allianz.QWQWQWQW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qwqwqwqw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qwqwqwqw2Application.class, args);
+	}
+
+}
